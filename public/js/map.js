2670152160ca5e9d36701efc459663e4178b1fd9
Akumar012 <kv44985@gamil.com>
2024-12-17 19:13:38 +0530

    satellite

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12",
+  style: "mapbox://styles/mapbox/satellite-streets-v12",
   center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 10, // starting zoom
 });
